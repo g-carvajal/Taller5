f6ac7e932232b8b584c3f28c75140c8335b23d56
Gabriela Carvajal <g.carvajal@uniandes.edu.co>
2024-11-15 11:12:23 +0000

    Initial commit

---
+++ webapp/Component.js
@@ -0,0 +1,12 @@
+sap.ui.define(
+    ["sap/suite/ui/generic/template/lib/AppComponent"],
+    function (Component) {
+        "use strict";
+
+        return Component.extend("namespace1.productsmanager2.Component", {
+            metadata: {
+                manifest: "json"
+            }
+        });
+    }
+);
